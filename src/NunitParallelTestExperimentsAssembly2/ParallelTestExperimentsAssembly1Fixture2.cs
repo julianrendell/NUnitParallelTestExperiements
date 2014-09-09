f22ce8537f2c2adf0c87f37a2c052ee0701c9451
Julian Rendell <juliangrendell@gmail.com>
2014-09-09 16:39:04 -0700

    test experiments

---
+++ src/NunitParallelTestExperimentsAssembly2/ParallelTestExperimentsAssembly1Fixture2.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace NunitParallelTestExperimentsAssembly2
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class ParallelTestExperimentsAssembly2Fixture2
+    {
+        private static void TestHelper(int delay, [CallerMemberName] string callerMemberName = "--unknown--")
+        {
+            TestContext.WriteLine("Starting Parallel Test {0}", callerMemberName);
+            TestContext.WriteLine("Waiting for {0}ms.", delay);
+            Thread.Sleep(delay);
+            TestContext.WriteLine("Finishing Parallel Test {0}", callerMemberName);
+
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest1_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest2_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest3_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest4_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest5_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+
+        [Test]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public void ParallelTest6_Assembly2_Fixture2()
+        {
+            TestHelper(5000);
+        }
+    }
+}
